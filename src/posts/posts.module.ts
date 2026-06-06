56f7b5f0e5f7fa77543debaad5cb1cdcd8b05b62
Barbara Arriagada <barriagad2025@alu.uct.cl>
2026-06-05 22:44:12 -0400

    style(posts): aplicación de reglas de linting y estandarización de código

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,33 +1,30 @@
-import { Module } from '@nestjs/common';
-import { PostsController } from './infrastructure/controllers/posts.controller';
-import { CreatePostUseCase } from './application/use-cases/create-post.use-case';
-import { PrismaPostRepository } from './infrastructure/repositories/prisma-post.repository';
-import { PrismaModule } from '../shared/prisma.module';
-import { PostsService } from './posts.service';
-import { FeedRankingStrategyFactory } from './feed-ranking.strategy';
+import { Module } from "@nestjs/common"
+import { PostsController } from "./infrastructure/controllers/posts.controller"
+import { CreatePostUseCase } from "./application/use-cases/create-post.use-case"
+import { PrismaPostRepository } from "./infrastructure/repositories/prisma-post.repository"
+import { PrismaModule } from "../shared/prisma.module"
+import { PostsService } from "./posts.service"
+import { FeedRankingStrategyFactory } from "./feed-ranking.strategy"
 
-import { ModerationModule } from '../moderation/moderation.module';
-import { ModerationAdapter } from './infrastructure/adapters/moderation.adapter';
+import { ModerationModule } from "../moderation/moderation.module"
+import { ModerationAdapter } from "./infrastructure/adapters/moderation.adapter"
 
 @Module({
-  imports: [
-    PrismaModule, 
-    ModerationModule
-  ],
-  controllers: [PostsController],
-  providers: [
-    CreatePostUseCase,
-    {
-      provide: 'IPostRepository',
-      useClass: PrismaPostRepository,
-    },
-    {
-      provide: 'IContentModerator',
-      useClass: ModerationAdapter,
-    },
-    PostsService,
-    FeedRankingStrategyFactory,
-  ],
-  exports: [PostsService],
+    imports: [PrismaModule, ModerationModule],
+    controllers: [PostsController],
+    providers: [
+        CreatePostUseCase,
+        {
+            provide: "IPostRepository",
+            useClass: PrismaPostRepository,
+        },
+        {
+            provide: "IContentModerator",
+            useClass: ModerationAdapter,
+        },
+        PostsService,
+        FeedRankingStrategyFactory,
+    ],
+    exports: [PostsService],
 })
 export class PostsModule {}
