0ce611c93a3c29ce43240af27b5e3543365ba3f0
Barbara Arriagada <barriagad2025@alu.uct.cl>
2026-06-05 21:24:41 -0400

    fix: integración final de IContentModerator con el servicio de Alan

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,23 +1,30 @@
-import { Module } from "@nestjs/common"
-import { PostsController } from "./infrastructure/controllers/posts.controller"
-import { CreatePostUseCase } from "./application/use-cases/create-post.use-case"
-import { PrismaPostRepository } from "./infrastructure/repositories/prisma-post.repository"
-import { PrismaModule } from "../shared/prisma.module"
-import { PostsService } from "./posts.service"
+import { Module } from '@nestjs/common';
+import { PostsController } from './infrastructure/controllers/posts.controller';
+import { CreatePostUseCase } from './application/use-cases/create-post.use-case';
+import { PrismaPostRepository } from './infrastructure/repositories/prisma-post.repository';
+import { PrismaModule } from '../shared/prisma.module';
+import { PostsService } from './posts.service';
+
+import { ModerationModule } from '../moderation/moderation.module';
+import { ModerationService } from '../moderation/moderation.service';
 
 @Module({
-    imports: [PrismaModule],
-    controllers: [PostsController],
-    providers: [
-        CreatePostUseCase,
-        {
-            provide: "IPostRepository",
-            useClass: PrismaPostRepository,
-        },
-        
-        PostsService,
-    ],
-
-    exports: [PostsService],
+  imports: [
+    PrismaModule, 
+    ModerationModule
+  ],
+  controllers: [PostsController],
+  providers: [
+    CreatePostUseCase,
+    {
+      provide: 'IPostRepository',
+      useClass: PrismaPostRepository,
+    },
+    {
+      provide: 'IContentModerator',
+      useExisting: ModerationService,
+    },
+    PostsService,
+  ],
 })
 export class PostsModule {}
